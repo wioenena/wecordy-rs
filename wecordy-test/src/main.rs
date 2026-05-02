52bba5477f84ff82a0f2ac2fd510408d3b9d7b7b
wioenena <wioenena@gmail.com>
2026-05-02 19:13:17 +0300

    chore(wecordy-test): update test binary to use new REST client and route API

--- wecordy-test/src/main.rs
+++ wecordy-test/src/main.rs
@@ -1,4 +1,20 @@
+use anyhow::Result;
+use wecordy_rest::ClientBuilder;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    run().await?;
+
+    Ok(())
+}
+
+async fn run() -> Result<()> {
+    let token = std::env::var("WECORDY_TOKEN")?;
+    let rest = ClientBuilder::new(token).build()?;
+    let route_meta = wecordy_api::routes::RouteMeta::get_user_by_id(10);
+    let x = route_meta.method();
+    let y: reqwest::Method = x.into();
+
+    dbg!(x, y);
+    Ok(())
 }
