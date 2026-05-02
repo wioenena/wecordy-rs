2d610d93b1331956480d91e9e85d296056f3ea08
wioenena <wioenena@gmail.com>
2026-05-02 19:13:47 +0300

    fix(wecordy-test): use free route function instead of removed RouteMeta method

--- wecordy-test/src/main.rs
+++ wecordy-test/src/main.rs
@@ -11,10 +11,7 @@ async fn main() -> Result<()> {
 async fn run() -> Result<()> {
     let token = std::env::var("WECORDY_TOKEN")?;
     let rest = ClientBuilder::new(token).build()?;
-    let route_meta = wecordy_api::routes::RouteMeta::get_user_by_id(10);
-    let x = route_meta.method();
-    let y: reqwest::Method = x.into();
-
-    dbg!(x, y);
+    let route_meta = wecordy_api::routes::get_user_by_id(10);
+    dbg!(route_meta);
     Ok(())
 }
