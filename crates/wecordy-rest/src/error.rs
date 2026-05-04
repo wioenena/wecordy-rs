ceabe969ef4d9d94d0c9fce5e4fb8975f8c6feee
wioenena <wioenena@gmail.com>
2026-05-05 00:34:12 +0300

    feat(rest): added detailed errors

--- crates/wecordy-rest/src/error.rs
+++ crates/wecordy-rest/src/error.rs
@@ -1,7 +1,19 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("reqwest error: {0}")]
-    Reqwest(#[from] reqwest::Error),
+    #[error("reqwest request error: {0}")]
+    ReqwestRequest(#[from] reqwest::Error),
+    #[error("reqwest header name error: {0}")]
+    ReqwestHeaderName(#[from] reqwest::header::InvalidHeaderName),
+    #[error("reqwest header value error: {0}")]
+    ReqwestHeaderValue(#[from] reqwest::header::InvalidHeaderValue),
+    #[error("cannot parse URL: {0}")]
+    UrlParse(#[from] url::ParseError),
+    #[error("unexpected header value: {0}")]
+    UnexpectedHeaderValue(String),
+    #[error("header missing: {0}")]
+    HeaderMissing(String),
+    #[error("reqwest response content unexpected")]
+    UnexpectedResponse,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
