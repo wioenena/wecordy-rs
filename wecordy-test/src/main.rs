829d9dfc79c24146e73ad3375b25f0c0baf9b3d7
wioenena <wioenena@gmail.com>
2026-05-05 02:23:04 +0300

    feat(test): add tracing and improve test client setup

--- wecordy-test/src/main.rs
+++ wecordy-test/src/main.rs
@@ -1,4 +1,9 @@
 use anyhow::Result;
+use std::time::Duration;
+use tracing::Level;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+use wecordy_api::models::ApiResponse;
+use wecordy_api::models::user::User;
 use wecordy_rest::ClientBuilder;
 
 #[tokio::main]
@@ -9,9 +14,24 @@ async fn main() -> Result<()> {
 }
 
 async fn run() -> Result<()> {
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info,wecordy_rest=debug"));
+
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .with_env_filter(env_filter)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let token = std::env::var("WECORDY_TOKEN")?;
-    let rest = ClientBuilder::new(token).build()?;
-    let route_meta = wecordy_api::routes::get_user_by_id(10);
-    dbg!(route_meta);
+    let rest = ClientBuilder::new(token)
+        .with_timeout(Duration::from_secs(3))
+        .with_base_url("https://gateway.wecordy.com/api/v1")
+        .build()?;
+    let user: ApiResponse<User> = rest
+        .get(wecordy_api::routes::get_current_user().path())
+        .await?;
+
+    dbg!(user);
     Ok(())
 }
