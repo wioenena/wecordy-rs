0350d00245ea1e7350e68515ff30d7a7531784f9
wioenena <wioenena@gmail.com>
2026-05-05 00:32:42 +0300

    feat(api): created struct for api responses

--- crates/wecordy-api/src/models/mod.rs
+++ crates/wecordy-api/src/models/mod.rs
@@ -1,3 +1,5 @@
+mod api_response;
+
 pub mod application;
 pub mod audit_log;
 pub mod channel;
@@ -7,3 +9,5 @@ pub mod message;
 pub mod role;
 pub mod server;
 pub mod user;
+
+pub use api_response::ApiResponse;
